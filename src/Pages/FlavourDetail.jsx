cf86693e6e63c63b333ad0fc34d4b03a23da28d1
krishnalh1909-design-tech-code <krishnalh1909@gmail.com>
2025-10-05 22:12:55 +0530

    name and details added

--- src/Pages/FlavourDetail.jsx
+++ src/Pages/FlavourDetail.jsx
@@ -18,7 +18,7 @@ const FlavourDetail = () => {
       window.scrollTo({ top: 0, behavior: "smooth" });
       document.documentElement.scrollTop = 0;
       document.body.scrollTop = 0;
-    }, 50); 
+    }, 50);
   }, [id]);
 
   if (!flavour) {
@@ -32,24 +32,28 @@ const FlavourDetail = () => {
       {/* Force remount video with key */}
       <div className="overflow-hidden" key={id}>
 
-        <Navbar/>
+        <Navbar />
 
         <div className="h-screen w-full overflow-hidden relative">
-           <video
-          className="h-screen w-full object-cover"
-          autoPlay
-          loop
-          muted
-          key={flavour.VidSrc} // ensures re-render
-        >
-          <source src={flavour.VidSrc} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+          <video
+            className="h-screen w-full object-cover"
+            autoPlay
+            loop
+            muted
+            key={flavour.VidSrc} // ensures re-render
+          >
+            <source src={flavour.VidSrc} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
         </div>
-        
 
-        <div className="font-[Antonio] absolute top-[15%] h-[30vh] w-full flex items-center justify-around">
-          <h1 className="text-[80px] font-bold text-[#FAEADE]">{flavour.name}</h1>
+
+        <div className="font-[Antonio] absolute top-[15%] h-max w-full flex items-center justify-between px-10">
+          <h1 className="text-[80px] font-bold text-[#FAEADE] w-[30vw]">{flavour.name}</h1>
+
+          <div className="h-full w-[25vw] text-[#FAEADE] font-[Antonio2] flex items-center text-right">
+            <p>{flavour.details}</p>
+          </div>
         </div>
 
         <div className="overflow-hidden">
